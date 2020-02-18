3a21390fe77fb4e3ce5df4f2218c0a96a923d73d
LucasMpa <lucasmpa16@gmail.com>
2020-02-18 13:40:45 -0300

    Finalização da Lógica (Inicio do Header)

--- App.js
+++ App.js
@@ -1,33 +1,84 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet} from 'react-native';
-import Params from './src/Params'
-import Field from './src/componentes/Field'
+import { Text, View, StyleSheet, Alert} from 'react-native';
+import MineField from './src/componentes/MineField'
+import Params from './src/Params';
+import params from './src/Params';
+import Header from './src/componentes/Header'
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed
+} from './src/Logic'
+import Field from './src/componentes/Field';
 
 export default class App extends Component {
+  
+  
+  constructor(props) { 
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+      won: false,
+      lost: false,
+    }
+  }
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Oh, you lost!')
+    } 
+    if (won) {
+      Alert.alert('Congratulations, you win')
+    }
+
+    this.setState({ board, lost, won})
+  }
+
+  onSelectedField = (row,column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board,row,column)
+    const won = wonGame(board)
+
+    if(won) {
+      Alert.alert('Congratulations, you win')
+    }
+    
+    this.setState({ board, won })
+
+  }
+  
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando Mines!</Text>
-        <Text style={styles.instructions}> Tamanho da grade:
-            {Params.getRowsAmount()} x{Params.getColumnsAmount()}</Text> 
-      
-
-        <Field />
-        <Field opened />
-        <Field opened mined />
-        <Field opened exploded />
-        <Field opened exploded mined />
-        <Field flagged/>
-        <Field opened nearMines={1}/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={4}/>
-        <Field opened nearMines={5}/>
-        <Field opened nearMines={6}/>
-        <Field opened nearMines={7}/>
-        <Field opened nearMines={8}/>
-        
-      </View>
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} 
+        onNewGame={() => this.setState(this.createState())} />
+        <View style={styles.board}></View>
+        <MineField board={this.state.board}
+          onOpenField={this.onOpenField}
+          onSelectedField={this.onSelectedField}/>
+        </View>     
     );
   }
 }
@@ -36,16 +87,12 @@ export default class App extends Component {
 const styles = StyleSheet.create({
    container: {
      flex: 1,
-     justifyContent: "center",
-     alignItems: "center",
-     //flexWrap: 'wrap',
-     //flexDirection: 'row'
-     
-   },
-   welcome: {
-     fontWeight: "bold",
-     fontSize: 35,
-     color: "black"
-   },
+     justifyContent: "flex-end",
+    },
+    board:{
+      alignItems: 'center',
+      backgroundColor: '#AAA'
+    },
+
 
 });
--- src/componentes/Flag.js
+++ src/componentes/Flag.js
@@ -5,10 +5,10 @@ import { StyleSheet, View} from 'react-native'
 export default props => {
     return(
         <View style={styles.container}>
-            <View style={styles.flagpole} />
-            <View style={styles.flag} />
-            <View style={styles.base1} />
-            <View style={styles.base2} />
+            <View style={[styles.flagpole, props.bigger ? styles.flagpoleBigger: null]} />
+            <View style={[styles.flag, props.bigger ? styles.flagBigger: null]} />
+            <View style={[styles.base1, props.bigger ? styles.base1Bigger: null]} />
+            <View style={[styles.base2, props.bigger ? styles.base2Bigger: null ]} />
         </View>
     )
 }
@@ -17,21 +17,21 @@ export default props => {
 const styles = StyleSheet.create({
 
     container:{
-        marginTop: 2,
+        marginTop: 8,
     },
     flagpole:{
         position: "absolute",
         height: 14,
         width: 2,
         backgroundColor: '#222',
-        marginLeft: 9,
+        marginLeft: 15,
     },
     flag:{
         position: 'absolute',
         height: 5,
         width: 6,
         backgroundColor: '#F22',
-        marginLeft: 3
+        marginLeft: 9
 
     },
     base1:{
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
         height: 2,
         width: 6,
         backgroundColor: '#222', 
-        marginLeft: 7,
+        marginLeft: 13,
         marginTop: 10,
 
     },
@@ -48,8 +48,35 @@ const styles = StyleSheet.create({
         height: 2,
         width: 10,
         backgroundColor: '#222',
-        marginLeft: 5,
+        marginLeft: 11,
         marginTop: 12,
+
+    },
+    //FlagBigger
+    
+    flagpoleBigger:{
+        height: 28,
+        width: 4,
+        marginLeft: 16,
+    },
+    flagBigger:{
+        height: 10,
+        width: 14,
+        marginLeft: 3
+
+    },
+    base1Bigger:{
+        height: 4,
+        width: 12,
+        marginLeft: 12,
+        marginTop: 20,
+
+    },
+    base2Bigger:{
+        height: 4,
+        width: 20,
+        marginLeft: 8,
+        marginTop: 24,
 
     },
 
